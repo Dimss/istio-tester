7d328fa364f285486ed07533202b1296fa8c7584
Dmitry Kartsev <dimssss@gmail.com>
2018-08-21 16:05:01 +0300

    update

---
+++ app_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestAverage(t *testing.T) {
+	v := 1
+	if v != 1 {
+		t.Error("Expected 1, got ", v)
+	}
+}
+
+func TestAverage1(t *testing.T) {
+	v := 1
+	if v != 1 {
+		t.Error("Expected 1, got ", v)
+	}
+}
