a9db161920fcfc1e6bdfb120d23a98e0889a3215
Dmitry Kartsev <dimssss@gmail.com>
2018-08-21 15:44:08 +0300

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh;
-cd /go
-ln -s /builds/dimss/my-test-proj/ ./
-ls -all
-cd /builds/dimss/my-test-proj
+ln -s /builds/dimss/my-test-proj/ /go/src/
+cd /go/src/my-test-proj
 dep ensure;
 export CGO_ENABLED=0 GOOS=linux GOARCH=amd64;
 go build -o istio-tester;
