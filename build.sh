8b56941e2196815512590e22a795a814bc87ac19
Dmitry Kartsev <dimssss@gmail.com>
2018-08-21 15:41:47 +0300

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
 curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh;
+cd /go
+ln -s /builds/dimss/my-test-proj/ ./
+ls -all
 cd /builds/dimss/my-test-proj
 dep ensure;
 export CGO_ENABLED=0 GOOS=linux GOARCH=amd64;
